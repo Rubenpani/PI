f3549b9fa4eea467edfba62ebf3944900d8f5a66
Rubén Paniagua Tascón <paniagua_ruben@hotmail.com>
2021-03-19 19:19:38 +0100

    añadido user_controller

---
+++ module/controllers/user_controller.php
@@ -0,0 +1,36 @@
+<?php 
+
+class userController {
+
+    private $propiedad = "";
+    
+    
+
+    public function set_propiedad($propiedad){
+
+        $this->propiedad = $propiedad;
+
+    }
+
+
+    public function get_propiedad(){
+
+        return $this->propiedad;
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+?>
